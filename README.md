ec0fe395557d85d9e5f493ac6cb690e8087edf70
PixelRaptor <sibandafreeman@gmail.com>
2020-03-22 00:57:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sdbbww-uqfepe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sdbbww-uqfepe)
---
+++ src/app/product-alert/product-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alert',
+  templateUrl: './product-alert.component.html',
+  styleUrls: ['./product-alert.component.css']
+})
+export class ProductAlertComponent {
+  @Input() product;
+  @Input() extras;
+  @Output() notify = new EventEmitter();
+
+}
